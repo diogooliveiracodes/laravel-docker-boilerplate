fbe7de324b956c95116dd475379057c3ffce022b
Diogo Oliveira <diogo.o@icloud.com>
2024-03-03 09:33:23 -0300

    add migrations to entrypoint

--- src/.docker/entrypoint.sh
+++ src/.docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ -f .env ]; then
+  php artisan migrate
   php artisan serve --host 0.0.0.0 --port 8000
 else
+  php artisan migrate --seed
   cat .env.example > .env
   composer update
   composer install
