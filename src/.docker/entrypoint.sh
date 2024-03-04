4ca9e1db9cad3e7be857167a72ba0309dae6402f
Diogo Oliveira <64746528+diogooliveiracodes@users.noreply.github.com>
2024-03-04 18:32:41 -0300

    Fix - Update entrypoint.sh

--- src/.docker/entrypoint.sh
+++ src/.docker/entrypoint.sh
@@ -3,10 +3,10 @@ if [ -f .env ]; then
   php artisan migrate
   php artisan serve --host 0.0.0.0 --port 8000
 else
-  php artisan migrate --seed
   cat .env.example > .env
   composer update
   composer install
+  php artisan migrate --seed
   php artisan key:generate
   php artisan serve --host 0.0.0.0 --port 8000
 fi
